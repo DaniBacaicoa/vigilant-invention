c8293055bf15f11eaed616b41c6876c7da24ab1f
Lorena Calvo Bartolomé <100330717@alumnos.uc3m.es>
2024-02-27 09:06:30 +0100

    First commit, main structure added.

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.keyword_generator import KeywordGenerator
+
+
+def main():
+    kw_generator = KeywordGenerator()
+    keys = kw_generator.get_keywords() # TODO: Implement me!
+
+
+if __name__ == '__main__':
+
+    main()
